72077024a099226bc4febf10388ae1514e7da511
shinchan1506 <31299638+shinchan1506@users.noreply.github.com>
2019-10-19 18:29:58 -0500

    ReportFragment created, jon-michael finish this please

---
+++ app/src/main/java/com/example/demonhacks/ReportFragment.java
@@ -0,0 +1,17 @@
+package com.example.demonhacks;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class ReportFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setRetainInstance(true);
+
+    }
+
+
+}

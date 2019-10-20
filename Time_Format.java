f64e5055ef83c39ec482896c867f77bfe4ba3bc9
agranados0121 <56767363+agranados0121@users.noreply.github.com>
2019-10-19 23:16:07 -0500

    Add files via upload
    Time Formatting

--- Time_Format.java
+++ Time_Format.java
@@ -1,31 +1,46 @@
 package demon;
 import java.time.*;
+import java.util.Arrays;
 
 public class Time_Format {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String time = getTime();
-		System.out.println(time);
+		String test = "2019-10-19T23:27:31";
+		String hour = test.substring(11,13);
+		String min = test.substring(14,16);
+		String[] time = new String[2];
+		time = getTime(hour, min);
+		System.out.println(Arrays.toString(time));
 	}
 
-	private static String getTime() {
-		String s = "";
+	private static String[] getTime(String arrival_hour, String arrival_min) {
+		String[] s = new String[2];
 		LocalTime tm = java.time.LocalTime.now();
-		LocalTime noon = LocalTime.NOON;
-		LocalTime mid = LocalTime.MIDNIGHT;
-		if(tm.equals(mid)) {
-			s = "12:00 AM";
+		int hr = Integer.parseInt(arrival_hour);
+		int min = Integer.parseInt(arrival_min);
+		
+		if(hr == 24) {
+			s[0] = String.format("Arival Time: %d:%s AM", (Integer.parseInt(arrival_hour)-12), arrival_min);
+			s[1] = String.format("Time Left: %d", arrival_min);
+			
+			
 		}
-		else if(tm.equals(noon)) {
-			s = "12:00 PM";
+		else if(hr == 12) {
+			s[0] = String.format("Arival Time: %d:%s PM", (Integer.parseInt(arrival_hour)-12), arrival_min);
+			s[1] = String.format("Time Left: %s", arrival_min);
 		}
-		else if(tm.isAfter(noon)) {
-			int t = tm.getHour()-12;
-			s = String.format("%d:%d PM", t, tm.getMinute());
+		else if(hr > 12 && hr < 24) {
+			int hr_remaining = java.lang.Math.abs(tm.getHour()- hr);
+			int min_remaining = java.lang.Math.abs(tm.getMinute()-min);
+			s[0] = String.format("Arival Time: %s:%s PM", (Integer.parseInt(arrival_hour)-12), arrival_min);
+			s[1] = String.format("Time Left: %d min" , min_remaining);
+			
 		}
-		else {
-			s = String.format("%d:%d AM", tm.getHour(), tm.getMinute());
+		else {//hr > 24 && hr < 12
+			int min_remaining = java.lang.Math.abs(tm.getMinute()-min);
+			s[0] = String.format("Arival Time: %s:%s AM", (Integer.parseInt(arrival_hour)-12), arrival_min);
+			s[1] = String.format("Time Left: %d min" , min_remaining);
 		}
 		return s;
 	}
